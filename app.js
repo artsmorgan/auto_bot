7241395aa08e1c0748f1405d65164a009750b583
Aletz Morgan <amorgan115@gmail.com>
2013-12-25 18:27:24 -0600

    update

--- app.js
+++ app.js
@@ -582,5 +582,5 @@ app.get('/getMotorcicleListPagination/', function(req, res){
 
 
 
-app.listen(5000);
-console.log('Listening on port 5000');
+app.listen(process.env.PORT || 5000)
+//console.log('Listening on port 5000');
