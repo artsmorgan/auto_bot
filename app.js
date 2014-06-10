2b1bd7985edf111d1376897796f095d9c514efb7
Aletz Morgan <amorgan115@gmail.com>
2014-06-10 08:12:08 -0600

    port fixed

--- app.js
+++ app.js
@@ -1167,6 +1167,9 @@ app.get('/vin/:vin', function(req, res){
     });
 
 });
+var ipaddr = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
-app.listen(process.env.PORT || 5000)
+app.listen(port)
+//app.listen(process.env.PORT || 5000)
 console.log('Listening on port 5000');
